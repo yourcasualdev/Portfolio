07da3a7e0f8b24278230cf14661689f99022bb14
Cenk Şengünlü <cenksengunlu@hotmail.com>
2022-02-23 23:15:37 +0300

    html changed

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -4,7 +4,7 @@ import { Route, Link } from 'react-router-dom'
 function Home() {
     return (
         <div className="content" style={{ alingItems: "center", textAlign: "center", paddingTop: "50px" }}>
-            <h1>Bu kısım daha bitmedi şuraya bakabilirsin <Link style={{ color: "blue", }} to="/projects">Projects</Link> </h1>
+            <h1>Fark etmez <Link style={{ color: "blue", }} to="/projects">Projects</Link> </h1>
         </div>
     )
 }
